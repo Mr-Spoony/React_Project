3fbdc5a6c28ce7e522b6929268f1b1d6fc07cbd4
Mr-Spoony <alexander.i.littlewood@gmail.com>
2022-11-29 19:14:36 +1300

    popup z-index

--- react_project/src/components/survivor.jsx
+++ react_project/src/components/survivor.jsx
@@ -10,8 +10,8 @@ class Survivor extends Component {
                     <span className={this.getBadgeClasses()}>{"Hunt XP: " + this.formatXp()}</span>
                     <button onClick={() => this.props.onIncrement(this.props.survivor.id)} className='fs-6 fw-normal fontFamily  btn btn-secondary btn-sm'>Add XP</button>
                     <button onClick={() => this.props.onDecrease(this.props.survivor.id)} className='fs-6 fw-normal fontFamily  btn btn-secondary btn-sm m-2'>Subtract XP</button>
-                    {this.props.survivor.data.alive === true && <button onClick={() => this.props.onStatus(this.props.survivor.id)} className='fs-6 fw-normal fontFamily  font-weight-bold btn btn-secondary btn-sm m-2'>Kill</button>}
-                    {this.props.survivor.data.alive === false && <button onClick={() => this.props.onStatus(this.props.survivor.id)} className='fs-6 fw-normal fontFamily  font-weight-bold btn btn-secondary btn-sm m-2'>Revive</button>}
+                    {this.props.survivor.data.alive === true && <button onClick={() => this.props.onStatus(this.props.survivor.id)} className='fs-6 fw-normal fontFamily font-weight-bold btn btn-secondary btn-sm m-2'>Kill</button>}
+                    {this.props.survivor.data.alive === false && <button onClick={() => this.props.onStatus(this.props.survivor.id)} className='fs-6 fw-normal fontFamily font-weight-bold btn btn-secondary btn-sm m-2'>Revive</button>}
                     {this.props.survivor.data.alive === false && <span className="fs-6 fw-normal fontFamily badge p-2 m-2 bg-danger">Dead</span>}
                     <button onClick={() => this.props.onDelete(this.props.survivor.id)} className='fs-6 fw-normal fontFamily  btn btn-warning btn-sm m-2'>Delete</button>
                 </span>
